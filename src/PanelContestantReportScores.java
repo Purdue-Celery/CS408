a6bec44d32387c030b04555e9096dd67db8f4814
mapan <mapan0817@gmail.com>
2013-09-12 19:34:21 -0400

    add panel for contestant report scores

--- src/PanelContestantReportScores.java
+++ src/PanelContestantReportScores.java
@@ -1,10 +1,45 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
-public class PanelContestantReportScores extends JPanel {
+public class PanelContestantReportRoundes extends JPanel implements ActionListener {
 	FrameMain f = null;
+	String[] columnNames = { "Round Name", "Period", "Year", "Test Time", "Test Room" };
 
-	public PanelContestantReportScores(FrameMain frameMain) {
+	JTable table;
+	JButton backButton = new JButton("Go back");
+	JScrollPane scroll;
+
+	public PanelContestantReportRoundes(FrameMain frameMain) {
 		f = frameMain;
 		setSize(FrameMain.WIDTH, FrameMain.HEIGHT);
+		this.setLayout(new BorderLayout());
+
+		backButton.setPreferredSize(new Dimension(this.getWidth(), 30));
+		this.add(backButton, BorderLayout.SOUTH);
+		backButton.addActionListener(this);
+	}
+
+	public void displayReport() {
+		String[][] data = f.getMyQueryManager().getRoundReportByContestant(f.getContestantID());
+		table = new JTable(data, columnNames);
+		scroll = new JScrollPane(table);
+		this.add(scroll, BorderLayout.CENTER);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		this.setVisible(false);
+		if (e.getSource() == backButton) {
+			// TODO clear panel
+			this.remove(table);
+			this.remove(scroll);
+			this.f.getPanelContestantMenu().setVisible(true);
+		}
 	}
 }
