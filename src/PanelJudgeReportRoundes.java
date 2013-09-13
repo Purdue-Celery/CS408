1eed8aa5d7fe120c2fc980b8bdf089baeb0559ad
RyanCccc <chenrd769@gmail.com>
2013-09-12 20:06:06 -0400

    Added Panel Judge Report

--- src/PanelJudgeReportRoundes.java
+++ src/PanelJudgeReportRoundes.java
@@ -1,10 +1,46 @@
-import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class PanelJudgeReportRoundes extends JPanel {
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class PanelJudgeReportRoundes extends JPanel implements ActionListener {
 	FrameMain f = null;
+	String[] columnNames = { "Round Name", "Test Time", "Test Room", "# of contestants", "# of evaluations", "# of notices" };
+
+	JTable table;
+	JButton backButton = new JButton("Go back");
+	JScrollPane scroll;
 
 	public PanelJudgeReportRoundes(FrameMain frameMain) {
 		f = frameMain;
 		setSize(FrameMain.WIDTH, FrameMain.HEIGHT);
+		this.setLayout(new BorderLayout());
+
+		backButton.setPreferredSize(new Dimension(this.getWidth(), 30));
+		this.add(backButton, BorderLayout.SOUTH);
+		backButton.addActionListener(this);
 	}
+
+	public void displayReport() {
+		String[][] data = f.getMyQueryManager().getRoundReportByJudge(f.getJudgeID());
+		table = new JTable(data, columnNames);
+		scroll = new JScrollPane(table);
+		this.add(scroll, BorderLayout.CENTER);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		this.setVisible(false);
+		if (e.getSource() == backButton) {
+			// TODO clear panel
+			this.remove(table);
+			this.remove(scroll);
+			this.f.getPanelJudgeMenu().setVisible(true);
+		}
+	}
+
 }
--- src/PanelJudgeReportScores.java
+++ src/PanelJudgeReportScores.java
@@ -1,10 +1,47 @@
-import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class PanelJudgeReportScores extends JPanel {
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class PanelJudgeReportScores extends JPanel implements ActionListener {
 	FrameMain f = null;
+
+	String[] columnNames = { "Round Name", "Period", "Year", "Contestant Name", "Current Score" };
+
+	JTable table;
+	JButton backButton = new JButton("Go back");
+	JScrollPane scroll;
 
 	public PanelJudgeReportScores(FrameMain frameMain) {
 		f = frameMain;
 		setSize(FrameMain.WIDTH, FrameMain.HEIGHT);
+		this.setLayout(new BorderLayout());
+
+		backButton.setPreferredSize(new Dimension(this.getWidth(), 30));
+		this.add(backButton, BorderLayout.SOUTH);
+		backButton.addActionListener(this);
+	}
+
+	public void displayReport() {
+		String[][] data = f.getMyQueryManager().getContestantScoreReportByJudge(f.getJudgeID());
+		table = new JTable(data, columnNames);
+		scroll = new JScrollPane(table);
+		this.add(scroll, BorderLayout.CENTER);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		this.setVisible(false);
+		if (e.getSource() == backButton) {
+			// TODO clear panel
+			this.remove(table);
+			this.remove(scroll);
+			this.f.getPanelJudgeMenu().setVisible(true);
+		}
 	}
 }
